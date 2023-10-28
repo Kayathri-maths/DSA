915618eae3fd4f78ab0e6f7bc66d6c985b2eb00e
Kayathri-maths <kayualagarsamy@gmail.com>
2023-10-28 22:23:09 +0530

    first commit

---
+++ arrayEqualOrNot.js
@@ -0,0 +1,29 @@
+class Solution {
+    //Function to check if two arrays are equal or not.
+    check(A,B,N){
+        // code here
+        let map=new Map();
+        
+        for(let a of A){
+            if(!map.has(a)){
+                map.set(a,1);
+            }else{
+            map.set(a,map.get(a)+1);
+            }
+        }
+        for(let b of B){
+            if(!map.has(b)){
+                map.set(b,1);
+            } else{
+            map.set(b,map.get(b)-1);
+            }
+        }
+        for(let [key,value] of map){
+            if(value!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+}
